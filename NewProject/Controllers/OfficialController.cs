8ce742613a63d86519daeefce259f1af3569f8b9
AkshayAchu4oo7 <achuirt@gmail.com>
2019-10-14 16:11:16 +0530

    Official for testing

---
+++ NewProject/Controllers/OfficialController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewProject.Controllers
+{
+    public class OfficialController : Controller
+    {
+        // GET: Official
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
